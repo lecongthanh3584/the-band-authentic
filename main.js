3916b7ad1b99031cc46372946f2e75523d2a472b
DESKTOP-NEA8RT1\Admin <thanh232002@gmail.com>
2022-01-11 15:02:12 +0700

    web the band

---
+++ main.js
@@ -0,0 +1,11 @@
+var x = 9;
+console.log('cong thanh', x);
+const a = 1 && NaN
+const b = 1 || NaN && 'Hi' || null && 100
+var d = " thanh";
+var e = 'cong';
+console.log('Ten toi la: ' + e + d);
+// var y = null && 'b' && null;
+// if(y)
+//     console.log("dung");
+// console.log('sai');       
